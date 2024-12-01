e65e693ae8f95e55e6adcfc7e2966aa271caf9b5
sguntupalli869 <76010709+sguntupalli869@users.noreply.github.com>
2024-11-30 23:26:30 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 // Your First Program
 class HelloWorld {
     public static void main(String[] args) {
-         System.out.println("Welcome to Git's Online Training with Edureka");
+         System.out.println("Welcome to Git's Online Training with BOW BOW Edureka");
     }
 }
